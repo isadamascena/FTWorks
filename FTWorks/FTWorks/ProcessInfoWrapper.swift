c28975020252423a9a2d46714b82553ee2e64f5f
isadamascena <idamasce@thoughtworks.com>
2018-09-29 13:42:21 -0300

    Adds Wrapper to native ProcessInfo static call

---
+++ FTWorks/FTWorks/ProcessInfoWrapper.swift
@@ -0,0 +1,9 @@
+//
+//  ProcessInfoWrapper.swift
+//  FTWorks
+//
+//  Created by Isabelly Damascena on 29/09/18.
+//  Copyright © 2018 Isabelly Damascena. All rights reserved.
+//
+
+import Foundation
---
+++ FTWorks/FTWorksTests/Mocks/ProcessInfoWrapperMock.swift
@@ -0,0 +1,9 @@
+//
+//  ProcessInfoWrapperMock.swift
+//  FTWorksTests
+//
+//  Created by Isabelly Damascena on 29/09/18.
+//  Copyright © 2018 Isabelly Damascena. All rights reserved.
+//
+
+import Foundation
