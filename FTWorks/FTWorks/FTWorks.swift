ae721e95ea192154ca62cc381d2db5f74c1df860
isadamascena <idamasce@thoughtworks.com>
2018-09-30 12:07:07 -0300

    Adds FTWorks and FTWorksTogggles to project [POSSIBILITY OF RED BUILD THHOUGH]

---
+++ FTWorks/FTWorks/FTWorks.swift
@@ -0,0 +1,13 @@
+//
+//  FTWorks.swift
+//  FTWorks
+//
+//  Created by Isabelly Damascena on 30/09/18.
+//  Copyright © 2018 Isabelly Damascena. All rights reserved.
+//
+
+import Foundation
+
+class FTWorks {
+    
+}
---
+++ FTWorks/FTWorksTests/FTWorksTest.swift
@@ -0,0 +1,18 @@
+//
+//  FTWorks.swift
+//  FTWorksTests
+//
+//  Created by Isabelly Damascena on 30/09/18.
+//  Copyright © 2018 Isabelly Damascena. All rights reserved.
+//
+
+import XCTest
+@testable import FTWorks
+
+class FTWorksTest {
+    
+    func testShouldConformWithFTWorksTogglesProtocol() {
+        let ftWorks = FTWorks()
+        XCTAssertNotNil(ftWorks as? FTWorksToggles)
+    }
+}
