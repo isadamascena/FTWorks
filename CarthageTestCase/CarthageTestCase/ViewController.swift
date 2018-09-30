b4b52d2a88360cc811b8ed0588e5170376c0eda9
isadamascena <idamasce@thoughtworks.com>
2018-09-30 19:19:38 -0300

    Adds CarthageTestCase to Repository

---
+++ CarthageTestCase/CarthageTestCase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarthageTestCase
+//
+//  Created by Isabelly Damascena on 30/09/18.
+//  Copyright © 2018 Isabelly Damascena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
