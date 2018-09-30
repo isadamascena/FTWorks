a5f7b41a5b1a84029f60405542110f34e3afefbf
isadamascena <idamasce@thoughtworks.com>
2018-09-30 18:34:09 -0300

    Creates Logger file to help debug

---
+++ FTWorks/FTWorks/Logger.swift
@@ -0,0 +1,21 @@
+//
+//  Logger.swift
+//  FTWorks
+//
+//  Created by Isabelly Damascena on 30/09/18.
+//  Copyright © 2018 Isabelly Damascena. All rights reserved.
+//
+
+import Foundation
+
+
+internal class Logger {
+    
+    static let logger = Logger()
+    
+    private init() {}
+    
+    func debug(_ message: String){
+        print(message)
+    }
+}
